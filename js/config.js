d6647737b4c4e89e68bd5f23c80df57e33c9f5ee
NoelMacwan <yulespencer@gmail.com>
2017-03-24 20:19:52 +0530

    stats: adapt for GlazeOS

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var configuration = {
-	name: 'Tesla OS', // required, string
-	endpointURL: 'https://teslaos.trackstats.space', // required, string
-	homepageURL: 'https://teslaos.github.io', // optional, string
+	name: 'Glaze OS', // required, string
+	endpointURL: 'https://ae0-cust-glazeos.corp.marv.pw', // required, string
+	homepageURL: 'https://glazeos.github.io', // optional, string
 	startDate: 'Sun, 21 Jun 2015 06:00:00 GMT', // optional, string
 	updateInterval: 1500, // optional, integer
 }
